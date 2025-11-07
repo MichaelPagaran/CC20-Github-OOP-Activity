510b99d5722357ab51897faa033bfb4ec0967b9b
Frances Ryle Tan <francesryletan0623@gmail.com>
2025-11-07 18:18:03 +0800

    Add SecondsAndMinutesConverter program

---
+++ programs/ryle/SecondsAndMinutesConverter.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class SecondsAndMinutesConverter {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter total seconds: ");
+        int totalSeconds = scanner.nextInt();
+
+        try {
+            TimeDuration duration = new TimeDuration(totalSeconds);
+            System.out.println("Converted time: " + duration.getDurationString());
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
+
+        scanner.close();
+    }
+}
+
+class TimeDuration {
+    private int hours;
+    private int minutes;
+    private int seconds;
+
+    public TimeDuration(int totalSeconds) {
+        if (totalSeconds < 0) {
+            throw new IllegalArgumentException("Invalid input: seconds cannot be negative.");
+        }
+        this.hours = totalSeconds / 3600;
+        this.minutes = (totalSeconds % 3600) / 60;
+        this.seconds = totalSeconds % 60;
+    }
+
+    public String getDurationString() {
+        return hours + "h " + minutes + "m " + seconds + "s";
+    }
+}

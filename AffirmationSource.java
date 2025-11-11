1093152d19be4d23f90b536e5a5487a06b98af99
Louiza Beatrice L. Lim <20230027697@my.xu.edu.ph>
2025-11-11 18:54:48 +0800

    Added abstract class AffirmationSource

---
+++ AffirmationSource.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+// --- ABSTRACTION ---
+abstract class AffirmationSource {
+    protected String currentAffirmation;
+
+    public abstract String getAffirmation(); // abstract method
+
+    public void displayAffirmation() {
+        System.out.println("🌞 Today's Affirmation: " + getAffirmation());
+    }
+}
